a76ab1897ff6687a6107ea11f170f64323a20e73
GitHub Action <action@github.com>
2024-02-16 09:43:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/maitriig3/understandcodeplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/maitriig3/understandcodeplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.maitriig3.understandcodeplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
